d231f4dd7eda56012b5e715e612e99de6a833f24
likaprui <pruidzelika7.gmail.com>
2023-08-30 15:42:46 +0400

    day 3 commit

---
+++ day 3/day3.py
@@ -0,0 +1,50 @@
+name = "lika"
+
+#print(len(name))   #4
+
+my_sentence = """გამარჯობა, შეგახსენებთ რომ სწავლას იწყებთ 15 სექტემბერს,
+გთხოვთ არ გააცდინოთ"""          #გრძელი სტრინგი
+
+#print(my_sentence)
+
+#knows_programming = True       bulean ტიპის ცვლადი
+
+#if 10>5:    #10>5 იგივეა რაც True
+    #print ("hello")
+    #tab_ის საშუალებით მოვახდინეთ ინდენტაცია (indentation)
+
+#full_name = "lika pruidze"
+#print(len(full_name))
+
+#ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად (0-დან ვიწყებთ დათვლას)
+#print(full_name[6])      
+#position = index სინონიმებია
+
+#print("z" not in full_name)
+
+full_name = "lika pruidze"
+#print(full_name[2:7])
+#print(full_name[2:7:2])
+               #start:finish:step
+
+#print(full_name[:5])         
+#print(full_name[5:])    #დასაწყისიდან მე-5-მდე, მეხუთედან დასასრულამდე
+
+#print(full_name[-1:-6:-1])
+
+#სტრინგებს აქვთ მეთოდები
+#  1) სტრინგ.upper()
+#  2) len(სტრინგ)
+
+# print(full_name.upper())                                                                    cntrl + ?
+
+name2 = "   LIKA   "
+#print(name2.lower())
+
+#print(name2.strip())         # ჩამოჭრა strip
+
+
+print(name2.replace(" ", "<3"))
+      
+
+
