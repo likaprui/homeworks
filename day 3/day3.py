ad840007c600b40cf9673570fa2af55691f84f62
likaprui <pruidzelika7.gmail.com>
2023-08-31 22:32:18 +0400

    day 3

--- day 3/day3.py
+++ day 3/day3.py
@@ -1,55 +1,74 @@
-name = "lika"
-
-#print(len(name))   #4
-
-my_sentence = """გამარჯობა, შეგახსენებთ რომ სწავლას იწყებთ 15 სექტემბერს,
-გთხოვთ არ გააცდინოთ"""          #გრძელი სტრინგი
-
-#print(my_sentence)
-
-#knows_programming = True       bulean ტიპის ცვლადი
-
-#if 10>5:    #10>5 იგივეა რაც True
-    #print ("hello")
-    #tab_ის საშუალებით მოვახდინეთ ინდენტაცია (indentation)
-
-#full_name = "lika pruidze"
-#print(len(full_name))
-
-#ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად (0-დან ვიწყებთ დათვლას)
-#print(full_name[6])      
-#position = index სინონიმებია
-
-#print("z" not in full_name)
-
-full_name = "lika pruidze"
-#print(full_name[2:7])
-#print(full_name[2:7:2])
-               #start:finish:step
-
-#print(full_name[:5])         
-#print(full_name[5:])    #დასაწყისიდან მე-5-მდე, მეხუთედან დასასრულამდე
-
-#print(full_name[-1:-6:-1])
-
-#სტრინგებს აქვთ მეთოდები
-#  1) სტრინგ.upper()
-#  2) len(სტრინგ)
-
-# print(full_name.upper())                                                                    cntrl + ?
-
-name2 = "   LIKA   "
-#print(name2.lower())
-
-#print(name2.strip())         # ჩამოჭრა strip
+#my_name = "lika"
+# my_sentence = "aa {1} bb {0} cc {2}".format("xx", my_name, "zz")
+# #ციფრებს თუ წავშლით გაჰყვება რიგს
+# print(my_sentence)
 
 
-print(name2.replace(" ", "<3"))
-print(full_name[0:9:5])
-print(full_name.upper()[5:])
-print("k" in full_name)
-print(len(full_name))
+# my_name = "lika"
+# my_surname = "pruidze"
+# my_age = 17
+# my_height = 165
+# my_sentence = "aa {} bb {} cc {} dd {}".format(my_name, my_surname, my_age, my_height)
 
-      
+# print(my_sentence)
+
+
+# if "e" in my_name:
+#     print("sheicavs e-s")
+# else:
+#     print("ar sheicavs e-s")
+
+#input
+#output
+
+# my_name = input("enter your name: ")
+# print("chemi saxelia " + my_name)
+
+# my_age = 17
+# my_age += 3
+# print(my_age)
+
+# name = input("enter your name : ")
+# surname = input("enter your surname: ")  #input-ით შემოდის მხოლოდ სტრინგი
+# age = input("enter your age: ")
+# print(" i am {}  {} my age is {} ".format(name, surname, age))
+
+
+# print(type(age))
+# new_age = int(age) + 3
+# print("after 3 years i am now {} years old".format(new_age))
+
+
+# firstnumber = input("enter your first number : ")
+# secondnumber = input("enter your second number: ")
+# message = input("you lost")
+
+# if firstnumber*secondnumber > 100 :
+#      print(firstnumber*secondnumber)
+# else :
+#      print(message)
+                                      #აქ დასაწერი მაქვს..........
+
+
+#print(20 % 6)   #გვიბრუნებს ნაშთს
+
+#input, %, if, +=
+
+#მომხმარებელმა ტერმინალიდან შემოიტანოს 3 რიცხვი
+#აქედან მხოლოდ კენტები შეიკრიბოს და დაპრინტოს ჯამი
+
+
+numb1 = int(input("enter your first number: "))
+numb2 = int(input("enter your second number: "))
+numb3 = int(input("enter your third number: "))
+product = int(numb1+numb2+numb3)
+
+if numb1 %2 > 0 and numb2 %2 > 0 and numb3 %2 > 0 :
+   print(product)
+
+
+
+
+ 
 
 
