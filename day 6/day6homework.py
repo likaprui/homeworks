002badc9f5f811c628f7b9c8885cd9f282099068
likaprui <pruidzelika7.gmail.com>
2023-09-08 15:21:47 +0400

    day 6

---
+++ day 6/day6homework.py
@@ -0,0 +1,6 @@
+my_list = ["mwvadi", "xinkali", "sacivi", "lobiani", "khachapuri"]
+price = [20, 2, 18, 4, 5]
+i = 0
+while i< len(my_list):
+    print(my_list[i]+ " "+ "girs" +" "+ str(price[i]) +" "+"lari")
+    i+=1
